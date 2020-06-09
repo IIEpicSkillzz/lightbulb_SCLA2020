a824c6ad5d450ae0d8737065490cdfaa7be7a094
TXT-54 <txt-54@macbook-pro-5.attlocal.net>
2020-06-09 10:18:40 -0700

    pppoopoo first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function pee() {
+    let bulbPic = document.getElementById("myBulb");
+      if(bulbPic.src.match("https://www.w3schools.com/js/pic_bulbon.gif")){
+      bulbPic.src="https://www.w3schools.com/js/pic_bulboff.gif";
+      } else {
+      bulbPic.src="https://www.w3schools.com/js/pic_bulbon.gif";
+      }
+    };
+    function epicGam(){
+      const butt = document.getElementById("lala");
+      alert("There is no meme uhhhh");
+     
+       
+    };
+    
